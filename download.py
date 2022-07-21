7833da010c17f6f44d6174fb3554b53270f00dc5
sasha375 <76862627+sasha375@users.noreply.github.com>
2022-07-21 12:12:57 +0300

    modified download image

--- download.py
+++ download.py
@@ -1,7 +1,9 @@
 import requests
 import argparse
 
-def download_image(url, imgname, params={}):
+def download_image(url, imgname, params=None):
+    if params == None:
+        params = {}
     image_content = requests.get(url, params=params).content
     
     with open(imgname, "bw") as f:
