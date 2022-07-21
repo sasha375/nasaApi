45dd2c1e0e58063c9fd4bd7fc15a9804a1527371
sasha375 <76862627+sasha375@users.noreply.github.com>
2022-07-21 11:40:26 +0300

    removed additional features

--- apod.py
+++ apod.py
@@ -6,17 +6,3 @@ def get_apod_urls(api_key, count):
                             params={"api_key": api_key, "count": count})
     response.raise_for_status()
     return [apod["url"] for apod in response.json()]
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Tool for downloading NASA APOD images.')
-    parser.add_argument("--count", default=1, help="Image count")
-    parser.add_argument("--api-key", type=str, help="NASA Api key", required=True)
-
-    args = parser.parse_args()
-
-    print(get_apod_urls(args.api_key, args.count))
-    
-if __name__ == "__main__":
-    main()
