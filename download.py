0aefad775d811399513b0d7d256b9dd3cf69cc86
sasha375 <76862627+sasha375@users.noreply.github.com>
2022-07-21 11:56:01 +0300

    Revert "removed additional features"
    This reverts commit 45dd2c1e0e58063c9fd4bd7fc15a9804a1527371.

--- download.py
+++ download.py
@@ -6,3 +6,19 @@ def download_image(url, imgname, params={}):
     
     with open(imgname, "bw") as f:
         f.write(image_content)
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Tool for downloading images.')
+    parser.add_argument('url', type=str, 
+                        help='Url to download', required=True)
+    parser.add_argument('path', type=str,
+                        help='Path to output image', required=True)
+    
+    args = parser.parse_args()
+
+    download_image(args.url, args.path)
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
--- epic.py
+++ epic.py
@@ -1,5 +1,6 @@
 import datetime
 import requests
+import argparse
 
 def build_url(url, params):
     if params:
@@ -18,3 +19,42 @@ def get_epic_image_ids(api_key, date):
 
 def get_epic_image_url(api_key, date, image_id):
     return "https://api.nasa.gov/EPIC/archive/natural/{}/png/{}.png".format(date, image_id), {"api_key":api_key}
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Tool for downloading NASA EPIC images.')
+    parser.add_argument('--get-dates', default=False,
+                        action="store_true", help='Get all dates (use only with api_key)')
+    parser.add_argument('--get-images', default=False,
+                        action="store_true", help='Get all images (required: date, api_key)')
+    parser.add_argument('--get-image-url', default=False,
+                        action="store_true", help='Get all images (required: date, image-id, api_key)')
+    parser.add_argument('--date', default=None, type=str, help='Date to download')
+    parser.add_argument('--image-id', default=None, type=str, help='Image id to download')
+    parser.add_argument('--api-key', type=str, help='NASA Api Key', required=True)
+    
+    args = parser.parse_args()
+    if not (args.get_dates or args.get_images or args.get_image_url):
+        parser.print_help()
+        print("ERROR: there is no get-dates, get-images or get-image-url flags")
+        exit(1)
+    if (args.get_dates and args.get_images) or (args.get_images and args.get_image_url) or (args.get_dates and args.get_image_url):
+        parser.print_help()
+        print("ERROR: there is two flags specified")
+        exit(1)
+    if args.get_dates:
+        print(get_epic_dates(args.api_key))
+    elif args.get_images:
+        if args.date == None:
+            parser.print_help()
+            print("ERROR: --date is required for --get-images")
+            exit(1)
+        print(get_epic_image_ids(args.api_key, args.date))
+    elif args.get_image_url:
+        if args.date == None or args.image_id == None:
+            parser.print_help()
+            print("ERROR: --date and --image_id is required for --get-images")
+            exit(1)
+        print(build_url(get_epic_image_url(args.api_key, args.date, args.image_id)))
+if __name__ == '__main__':
+    main()
