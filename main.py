7b14d2b52f7a5521fd9377f9813f993047380720
sasha375 <76862627+sasha375@users.noreply.github.com>
2022-07-21 12:39:57 +0300

    added *nix paths

--- main.py
+++ main.py
@@ -25,14 +25,14 @@ def main():
     
     for index, image_url in enumerate(apod.get_apod_urls(NASA_API_KEY, 40)):
         extention = os.path.splitext(image_url)[1]
-        filename = "images/nasa_apod_{}{}".format(index, extention)
+        filename = os.path.join("images", "nasa_apod_{}{}".format(index, extention))
         print("Downloading", filename)
         download.download_image(image_url, filename)
         imageList.append(filename)
         
     for index, image_url in enumerate(spacex.get_flight_images("5eb87d47ffd86e000604b38a")):
         extention = os.path.splitext(image_url)[1]
-        filename = "images/spacex{}{}".format(index, extention)
+        filename = os.path.join("images", "nasa_spacex_{}{}".format(index, extention))
         print("Downloading", filename)
         download.download_image(image_url, filename)
         imageList.append(filename)
