2156c1ed44224457134342ef828a62e92abd59e3
stacksofblue5 <s.kk.kravchenko@yandex.ru>
2022-08-31 11:56:44 +0300

    fixed APOD

--- apod.py
+++ apod.py
@@ -9,7 +9,7 @@ def get_apod_urls(api_key, count):
     response = requests.get("https://api.nasa.gov/planetary/apod",
                             params={"api_key": api_key, "count": count})
     response.raise_for_status()
-    return [apod["url"] for apod in response.json()]
+    return [apod["url"] for apod in response.json() if apod["media_type"]=="image"]
 
 
 def main():
