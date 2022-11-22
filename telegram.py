e14adc59f549f257fe3af2a341c8c0641eda2736
stacksofblue5 <s.kk.kravchenko@yandex.ru>
2022-11-22 17:21:34 +0300

    remove args

--- telegram.py
+++ telegram.py
@@ -15,11 +15,10 @@ def send_image(bot, channelId, photoPath):
 
 def main():
     load_dotenv(".secure/.env")
+    load_dotenv(".env")
 
     parser = argparse.ArgumentParser(
         description='Tool for sending images to telegram.')
-    parser.add_argument('--channel-id', type=str, 
-                        help='Telegram channel id', required=True)
     parser.add_argument('--image-path', type=str,
                         help='Image path or glob mask', default="images")
     
@@ -27,7 +26,7 @@ def main():
 
     bot = init_bot(os.environ["TELEGRAM_TOKEN"])
     images = glob.glob(args.image_path)
-    send_image(bot, args.channel_id, random.choose(images))
+    send_image(bot, os.environ["TELEGRAM_CHANNEL_ID"], random.choose(images))
 
 if __name__ == "__main__":
     main()
